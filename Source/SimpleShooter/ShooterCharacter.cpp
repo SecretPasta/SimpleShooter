1bc63cf2952809f0c64887ac960f5925cd89b072
SecretPasta <boris.shoostin@gmail.com>
2023-09-08 14:47:23 +0300

    Added Keybindings to control the character

--- Source/SimpleShooter/ShooterCharacter.cpp
+++ Source/SimpleShooter/ShooterCharacter.cpp
@@ -29,6 +29,29 @@ void AShooterCharacter::Tick(float DeltaTime)
 void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"),this, &AShooterCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &AShooterCharacter::MoveRight);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &AShooterCharacter::LookUp);
+	PlayerInputComponent->BindAxis(TEXT("LookRight"), this, &AShooterCharacter::LookRight);
+	PlayerInputComponent->BindAction(TEXT("JUMP"), EInputEvent::IE_Pressed, this, &AShooterCharacter::Jump);
 }
 
+void AShooterCharacter::MoveForward(float AxisValue) {
+	AddMovementInput(GetActorForwardVector()* AxisValue);
+}
+
+void AShooterCharacter::MoveRight(float AxisValue) {
+	AddMovementInput(GetActorRightVector()*AxisValue);
+}
+
+void AShooterCharacter::LookUp(float AxisValue) {
+	AddControllerPitchInput(AxisValue);
+}
+
+void AShooterCharacter::LookRight(float AxisValue) {
+	AddControllerYawInput(AxisValue);
+}
+
+void AShooterCharacter::Jump() {
+	ACharacter::Jump();
+}
