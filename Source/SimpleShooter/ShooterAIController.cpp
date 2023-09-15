68a937e2dd1cfbe02fd8ff505dc4333963e5e085
SecretPasta <boris.shoostin@gmail.com>
2023-09-15 20:13:25 +0300

    Moved all AI functionality into Services C++ classes
    Moved code from Tick in AI controller to 2 seprate C++ class services to be used in behavior tree

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -20,14 +20,4 @@ void AShooterAIController::BeginPlay() {
 
 void AShooterAIController::Tick(float DeltaSeconds){
 	Super::Tick(DeltaSeconds);
-
-	if (LineOfSightTo(PlayerPawn)) {
-		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
-		GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnowPlayerLocation"), PlayerPawn->GetActorLocation());
-	}
-	else {
-		GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
-	}
-
-	
 }
