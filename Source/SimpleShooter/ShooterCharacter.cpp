96df28c77c702a2f733fd3e39d179a678ccf2a95
SecretPasta <boris.shoostin@gmail.com>
2023-09-16 14:23:52 +0300

    Added Health Bar

--- Source/SimpleShooter/ShooterCharacter.cpp
+++ Source/SimpleShooter/ShooterCharacter.cpp
@@ -72,6 +72,11 @@ bool AShooterCharacter::IsDead() const
 	return Health <= 0;
 }
 
+float AShooterCharacter::GetHealthPercent() const
+{
+	return Health / MaxHealth;
+}
+
 
 void AShooterCharacter::MoveForward(float AxisValue) {
 	AddMovementInput(GetActorForwardVector() * AxisValue * Speed);
